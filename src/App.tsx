3ecb127ad796a1b1c81e5d3f010783728df289ec
Githubname-amin <amin15279402577@gmail.com>
2024-07-03 17:44:48 +0800

    feat:搭建完毕，开始实现功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Layout, Menu, MenuProps } from "antd";
+import { HomeOutlined, PlayCircleOutlined } from "@ant-design/icons";
+import Index from "./components/index";
 
+type MenuItem = Required<MenuProps>["items"][number];
+type MenuCurrent = "index" | "webrtc";
+const items1: MenuItem[] = [
+  {
+    label: "主页",
+    key: "index",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: "音视频相关",
+    key: "webrtc",
+    icon: <PlayCircleOutlined />,
+    children: [
+      { label: "拍照", key: "takePhoto" },
+      { label: "录制", key: "transcribe" },
+    ],
+  },
+];
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  backgroundColor: "#fff",
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [current, setCurrent] = useState<MenuCurrent>("index");
+  const { Header, Content } = Layout;
+
+  const handleMenuClick = (e: any) => {
+    setCurrent(e.key);
+  };
+  useEffect(() => {
+    console.log("?", current);
+    return () => {};
+  }, [current]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="mainBox">
+      <Layout>
+        <Header style={headerStyle}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            selectedKeys={[current]}
+            onClick={handleMenuClick}
+            defaultSelectedKeys={["2"]}
+            items={items1}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={contentStyle}>
+          <div className="content">{current === "index" && <Index />}</div>
+        </Content>
+      </Layout>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index/index.tsx
@@ -0,0 +1,8 @@
+const index = () => {
+  return <>
+    <div>
+        index页面
+    </div>
+  </>;
+};
+export default index;
