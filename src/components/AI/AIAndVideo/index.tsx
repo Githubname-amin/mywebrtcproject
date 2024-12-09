271af46626214748c2672d089ff3a2a1578e7a02
aminPC <wangmin010504@gmail.com>
2024-12-10 05:17:05 +0800

    feat:大概的写了写，现在写转录

--- src/components/AI/AIAndVideo/index.tsx
+++ src/components/AI/AIAndVideo/index.tsx
@@ -1,16 +1,22 @@
-import { Button, Card, Layout, message, Upload } from "antd";
+import { Button, Card, Layout, message, Table, Upload } from "antd";
 import "./index.less";
 // import ShowVideo from "./Components/showVideo";
 
-import { UploadOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  SyncOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
 import { RcFile } from "antd/es/upload";
 import { useRef, useState } from "react";
 
 const AIAndVideo = () => {
   // data-----------------------------
   const [uploadedFiles, setUploadedFiles] = useState<any[]>([]);
+  const [selectedFiles, setSelectedFiles] = useState([]); // 存储选中的文件
   const [mediaUrl, setMediaUrl] = useState(null);
   const mediaRef = useRef(null);
+  const [currentFile, setCurrentFile] = useState(null); // 当前预览的文件
 
   // Function===================================
   // 用来上传文件
@@ -43,11 +49,50 @@ const AIAndVideo = () => {
     setUploadedFiles((prev) => [...prev, newFile]);
     // 如果是第一个文件，动设置为当前预览文件
     if (uploadedFiles.length === 0) {
-      // setCurrentFile(newFile);
+      setCurrentFile(newFile);
       setMediaUrl({ url, type: isVideo ? "video" : "audio" });
     }
     return false; // 阻止自动上传
   };
+
+  // 删除文件
+  const handleFileDelete = (fileId: any) => {
+    setUploadedFiles((prev) => prev.filter((f) => f.id !== fileId));
+  };
+
+  // 删除选中的文件
+  const handleDeleteSelectedFiles = () => {
+    setUploadedFiles((prev) =>
+      prev.filter((f) => !selectedFiles.includes(f.id))
+    );
+    setSelectedFiles([]); // 清空选中的文件
+    // 如果删除的是当前预览文件，自动展示下一个文件
+    if (currentFile && selectedFiles.includes(currentFile.id)) {
+      const remainingFiles = uploadedFiles.filter(
+        (file) => !selectedFiles.includes(file.id)
+      );
+      const nextFile = remainingFiles[0];
+      if (nextFile) {
+        setCurrentFile(nextFile);
+        setMediaUrl({ url: nextFile.url, type: nextFile.type });
+      } else {
+        setCurrentFile(null);
+        setMediaUrl(null);
+      }
+    }
+  };
+
+  // 预览
+  const handleFilePreview = (file: any) => {
+    const currentFileRef = uploadedFiles.find((f) => f.id === file.id);
+    setCurrentFile(currentFileRef);
+    setMediaUrl({ url: file.url, type: file.type });
+  };
+  // 处理文件选择
+  const handleFileSelect = (fileIds: any) => {
+    setSelectedFiles(fileIds);
+  };
+
   return (
     <>
       <Layout style={{ minHeight: "90vh", background: "#f0f2f5" }}>
@@ -76,7 +121,7 @@ const AIAndVideo = () => {
             </div>
             {/* 用来上传视频 */}
             <div className="upload">
-              <Card style={{ width: 400, height: 400 }}>
+              <Card style={{ width: 400, height: 100 }}>
                 <div className="upload-section">
                   <Upload
                     beforeUpload={handleUpload}
@@ -87,6 +132,112 @@ const AIAndVideo = () => {
                   >
                     <Button icon={<UploadOutlined />}>上传本地文件</Button>
                   </Upload>
+                  <Button
+                    className="deleteBtn"
+                    style={{ width: 100 }}
+                    icon={<UploadOutlined />}
+                    onClick={() => handleDeleteSelectedFiles(selectedFiles)}
+                  >
+                    删除选中
+                  </Button>
+                  <Button
+                    className="transcriptBtn"
+                    style={{ width: 100 }}
+                    icon={<UploadOutlined />}
+                  >
+                    转录选中
+                  </Button>
+
+                  {/* <div className="upload-tips">支持多个视频和音频文件格式</div> */}
+                </div>
+              </Card>
+              <Card style={{ width: 400, height: 280 }}>
+                <div>
+                  <Table
+                    dataSource={uploadedFiles}
+                    rowSelection={{
+                      selectedRowKeys: selectedFiles,
+                      onChange: handleFileSelect,
+                      preserveSelectedRowKeys: true,
+                    }}
+                    rowKey="id"
+                    size="small"
+                    columns={[
+                      {
+                        title: "文件名",
+                        dataIndex: "name",
+                        key: "name",
+                        width: "70%",
+                      },
+                      {
+                        title: "类型",
+                        dataIndex: "type",
+                        key: "type",
+                        render: (type) => (type === "video" ? "视频" : "音频"),
+                      },
+                      {
+                        title: "状态",
+                        dataIndex: "status",
+                        key: "status",
+                        render: (status) => {
+                          switch (status) {
+                            case "waiting":
+                              return "等待转录";
+                            case "transcribing":
+                              return (
+                                <>
+                                  <SyncOutlined spin /> 转录中
+                                </>
+                              );
+                            case "done":
+                              return (
+                                <span style={{ color: "#52c41a" }}>已完成</span>
+                              );
+                            case "error":
+                              return (
+                                <span style={{ color: "#ff4d4f" }}>失败</span>
+                              );
+                            case "interrupted":
+                              return (
+                                <span style={{ color: "#faad14" }}>
+                                  转录中断
+                                </span>
+                              );
+                            default:
+                              return status;
+                          }
+                        },
+                      },
+                      {
+                        title: "操作",
+                        dataIndex: "action",
+                        key: "action",
+                        render: (_, record) => (
+                          <Button
+                            type="text"
+                            danger
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              handleFileDelete(record.id);
+                            }}
+                            icon={<DeleteOutlined />}
+                            disabled={record.status === "transcribing"}
+                          >
+                            删除
+                          </Button>
+                        ),
+                      },
+                    ]}
+                    onRow={(record) => ({
+                      onClick: () => handleFilePreview(record),
+                      style: {
+                        cursor: "pointer",
+                        background:
+                          currentFile?.id === record.id ? "#e6f7ff" : "inherit",
+                      },
+                    })}
+                    pagination={false}
+                  ></Table>
                 </div>
               </Card>
             </div>
